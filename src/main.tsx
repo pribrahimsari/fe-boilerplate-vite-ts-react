01dce4c79b5312edd14e1fb3b55c4b676e00f00b
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-09-06 15:59:38 +0300

    add material ui v5 and recommended globals to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+// Material UI's default typography configuration relies only on the 300, 400, 500, and 700 font weights.
+//   ref: https://mui.com/material-ui/getting-started/installation/
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+// MUI: Global Reset
+import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
