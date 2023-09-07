35a88bfe2fb2610eca0b5353b427133d4731ccba
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-09-07 16:23:30 +0300

    delete product logic

--- src/types/ProductTableRow.tsx
+++ src/types/ProductTableRow.tsx
@@ -1,6 +1,10 @@
 import { Product } from "src/types/ProductTypes.ts";
-import { TableCell, TableRow } from "@mui/material";
+import { IconButton, TableCell, TableRow } from "@mui/material";
 import { makeStyles } from "tss-react/mui";
+import { Add, Delete, Edit } from "@mui/icons-material";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useSnackbar } from "notistack";
 
 const useStyles = makeStyles()(() => ({
   row: {
@@ -10,6 +14,20 @@ const useStyles = makeStyles()(() => ({
 
 const ProductTableRow = ({ product }: { product: Product }) => {
   const { classes } = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const deleteM = useMutation(
+    ["deleteProduct"],
+    (id: number): Promise<Product> => {
+      const url = `https://dummyjson.com/products/${id}`;
+      return axios.delete(url);
+    },
+    {
+      onSuccess: () => {
+        enqueueSnackbar("Deleted");
+      },
+    }
+  );
 
   return (
     <TableRow hover className={classes.row}>
@@ -17,7 +35,17 @@ const ProductTableRow = ({ product }: { product: Product }) => {
       <TableCell>{product.title}</TableCell>
       <TableCell>{product.price}</TableCell>
       <TableCell>{product.category}</TableCell>
-      <TableCell>TODO</TableCell>
+      <TableCell>
+        <IconButton>
+          <Add />
+        </IconButton>
+        <IconButton>
+          <Edit />
+        </IconButton>
+        <IconButton disabled={deleteM.isLoading}>
+          <Delete onClick={() => deleteM.mutate(product.id)} />
+        </IconButton>
+      </TableCell>
     </TableRow>
   );
 };
